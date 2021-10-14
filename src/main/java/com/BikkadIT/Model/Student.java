8d048c9a76e610a3a00e72b7da712edff3307986
Ashwin <ashwinnagelwar@gmail.com>
2021-10-14 12:50:28 +0530

    SCO-1: project structure created and model class

---
+++ src/main/java/com/BikkadIT/Model/Student.java
@@ -0,0 +1,48 @@
+package com.BikkadIT.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int id;
+	String name;
+	String address;
+	int age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", address=" + address + ", age=" + age + "]";
+	}
+	
+	
+	
+}
